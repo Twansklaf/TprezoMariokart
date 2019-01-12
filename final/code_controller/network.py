008311fe8ebba1e62a64033bcb9026bfe4bded77
twans <antoine.dolant@u-psud.fr>
2019-01-12 11:48:43 +0100

    fixed

---
+++ final/code_controller/network.py
@@ -0,0 +1,20 @@
+from struct import pack
+from struct import unpack
+from struct import calcsize as sizeof
+
+
+CONNECT_TO_CAR = 1
+MOVE_CAR = 2
+GET_MALUS = 3
+SEND_MALUS = 4
+
+
+# Cree un paquet avec le code du type de message
+def create_packet(op_code, data):
+    return pack("!I{}s".format(len(data)), op_code, data.encode())
+
+
+# Extrait le code du type de message ainsi que les donnees du paquet
+def process_packet(packet):
+    return unpack("!I{}s".format(len(packet)-sizeof("!I")), packet)
+
